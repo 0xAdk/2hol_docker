2cff28053a6fa409355d3c0c2291d258f828e5a2
0xadk <0xadk@users.noreply.github.com>
2021-12-29 12:09:54 -0800

    create server_data's main sub directories if they don't exist
    This allows you to bind mount to only the /server_data directory;
    previously this would break since the bind mount would removed the
    required directories (data, cache, and log)

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# make sure main server_data directories exist
+mkdir -p /server_data/data /server_data/cache /server_data/log
+
 # create directory at each symlink's target if it doesn't already exist
 mkdir -p $(readlink -f curseLog failureLog foodLog lifeLog mapChangeLogs)
 

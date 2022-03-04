a768e05c59cf1933a0279e24536f8133d7166a74
0xadk <0xadk@users.noreply.github.com>
2022-03-03 21:20:18 -0800

    generalize config moving code into a function
    Since the "settings" directory wasn't the only configuation that comes
    with the server by default I made the code that symlinked / moved the
    default config to the server data config directory into a general
    function.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env sh
 
+redirect_to_config() {
+	FILE="$1"
+
+	# the file is already a symlink, we don't have to move anything
+	if [ -L "$FILE" ]; then
+		return
+	fi
+
+	# place the file in server config if it doesn't already exist
+	if [ ! -e "/server_data/config/${FILE}" ] && [ -e "${FILE}" ]; then
+		mv "${FILE}" "/server_data/config/${FILE}"
+	fi
+
+	# replace the file with a symlink to the file in the servers config directory
+	rm -r "${FILE}"
+	ln -s "/server_data/config/${FILE}"
+}
+
+
 # make sure main server_data directories exist
 mkdir -p /server_data/data /server_data/cache /server_data/log /server_data/config
 
 # create directory at each symlink's target if it doesn't already exist
 mkdir -p $(readlink -f curseLog failureLog foodLog lifeLog mapChangeLogs)
 
-# make sure settings is symlinked into the server_data/config directory
-## if settings is already a symlink skip this
-if [ ! -L settings ]; then
-	# if we don't already have settings directory in our server data directory
-	# take the (presumably default) settings from the docker file
-	if [ ! -d /server_data/config/settings ]; then
-		mv settings /server_data/config/settings
-	fi
-
-	# replace the default settings directory with a symlink to server's setting directory
-	rm -r settings
-	ln -s /server_data/config/settings
-fi
+# make sure settings are exposed through the config directory
+redirect_to_config settings
 
 # make stdout and stderr line buffered
 exec stdbuf -oL -eL "$@"
